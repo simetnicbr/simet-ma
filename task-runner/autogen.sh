f3a98d4e02443c1bad6018efb2e545314c3cac2e
Henrique de Moraes Holschuh <henrique@nic.br>
2018-08-24 16:58:14 -0300

    [MDC-198] simet-ma-runner: rename and autotoolize
    * Rename the task-runner executable to simet-ma_run.sh
    * Use GNU automake
    * Tie to the toplevel

---
+++ task-runner/autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Commands required to prepare tree for building after
+# a clean checkout
+#
+# Embedded libubox build dependencies:
+#    libjson-c-dev
+#    liblua5.1-dev
+#    cmake
+#
+# Main project build dependencies
+#    autoconf, automake, libtool (newer possible)
+
+autoreconf -i
