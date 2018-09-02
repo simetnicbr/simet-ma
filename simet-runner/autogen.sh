e7368be22cbf01e343f98d955ce5bc547c37d6ba
Henrique de Moraes Holschuh <henrique@nic.br>
2018-09-02 15:56:05 -0300

    [MDC-205] all: handle missing git worktree for version
    During an openwrt build, and sometimes during a tarball build,
    it is simply not possible to get the version from the VCS.
    
    In that case, the version *must* have been enshrined into a "version"
    file in the top_srcdir.   If it isn't, abort the build.
    
    Make bump-version at the top-level generates a new version file.
    Alternatively, rm -f version ; make version will also work.
    
    The version file should be updated, and then tagged.

--- simet-runner/autogen.sh
+++ simet-runner/autogen.sh
@@ -11,4 +11,8 @@
 # Main project build dependencies
 #    autoconf, automake, libtool (newer possible)
 
+./version.sh >/dev/null 2>&1 || {
+	echo "$0: missing version file and not in a worktree" >&2
+	exit 1
+}
 autoreconf -i
