b7fde40d4a57168ddf0f590f6240a6682d741808
Fabio Nokai <fnokai@nic.br>
2018-09-21 11:30:08 -0300

    [MDC-234] change report column order

--- tcpbw-client-c/src/report.h
+++ tcpbw-client-c/src/report.h
@@ -3,12 +3,13 @@
 
 #include "json-c/json.h"
 
-typedef struct tcp_download_result {
+typedef struct tcpbw_download_result
+{
     uint64_t bytes;
     uint64_t interval; /* microseconds */
     unsigned int nstreams;
 } DownResult;
 
-json_object * createReport(json_object*, DownResult*, uint32_t); 
+json_object *createReport(json_object *, DownResult *, uint32_t);
 
 #endif /* REPORT_H_ */
