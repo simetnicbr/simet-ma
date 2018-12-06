c877df746f2c88f53436fef52806ff6042c8bd0c
Henrique de Moraes Holschuh <henrique@nic.br>
2018-12-06 12:09:39 -0200

    [MDC-392] simet-ma: twamp: add TEST socket information metric
    Add a result table with the ambient metrics for the TEST socket to the
    output.

--- twamp-client-c/src/report.c
+++ twamp-client-c/src/report.c
@@ -22,14 +22,45 @@
 
 #include <json-c/json.h>
 #include <assert.h>
+#include <errno.h>
+
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
 
 #define TWAMP_R_NUMCOLS 8
-const char const * twamp_report_col_names[TWAMP_R_NUMCOLS] = {
+const char * const twamp_report_col_names[TWAMP_R_NUMCOLS] = {
 	"senderSeqNum", "reflectorSeqNum", "receiverSeqNum",
 	"senderTimeUs", "reflectorRecvTimeUs",
 	"reflectorSendTimeUs", "receiverTimeUs",
 	"rttUs"
 };
+
+enum {
+    socket_tbl_col_observer = 0,
+    socket_tbl_col_local_af,
+    socket_tbl_col_local_addr,
+    socket_tbl_col_local_port,
+    socket_tbl_col_remote_af,
+    socket_tbl_col_remote_addr,
+    socket_tbl_col_remote_port,
+    SOCK_TBL_COL_MAX
+};
+const char * const socket_report_col_names[SOCK_TBL_COL_MAX] = {
+    [socket_tbl_col_observer] = "observer",
+    [socket_tbl_col_local_af] = "local-address-family",
+    [socket_tbl_col_local_addr] = "local-address",
+    [socket_tbl_col_local_port] = "local-port",
+    [socket_tbl_col_remote_af] = "remote-address-family",
+    [socket_tbl_col_remote_addr] = "remote-address",
+    [socket_tbl_col_remote_port] = "remote-port",
+};
+
+struct twamp_report_private {
+    json_object *root;
+}; /* TWAMPReport->privdata */
 
 static void xx_json_object_array_add_uin64_as_str(json_object *j, uint64_t v)
 {
@@ -38,12 +69,166 @@ static void xx_json_object_array_add_uin64_as_str(json_object *j, uint64_t v)
         json_object_array_add(j, json_object_new_string(buf));
 }
 
+static const char *str_ip46(int ai_family)
+{
+    switch (ai_family) {
+    case AF_INET:
+        return "ip4";
+    case AF_INET6:
+        return "ip6";
+    }
+    return "ip";
+}
+
+
+static int xx_nameinfo(struct sockaddr_storage *sa, socklen_t sl,
+                        sa_family_t *sa_f, const char **family,
+                        const char **hostname, const char **hostport)
+{
+    char namebuf[256], portbuf[32];
+    assert(hostname && hostport);
+
+    if (sa->ss_family == AF_UNSPEC || getnameinfo((struct sockaddr *)sa, sl,
+                                            namebuf, sizeof(namebuf), portbuf, sizeof(portbuf),
+					    NI_NUMERICHOST | NI_NUMERICSERV)) {
+        if (sa_f)
+            *sa_f = AF_UNSPEC;
+        if (family)
+           *family = strdup("unknown");
+        *hostname = strdup("unknown");
+        *hostport = strdup("error");
+        return 1;
+    }
+
+    *hostname = strdup(namebuf);
+    *hostport = strdup(portbuf);
+    if (sa_f)
+        *sa_f = sa->ss_family;
+    if (family)
+        *family = strdup(str_ip46(sa->ss_family));
+
+    return 0;
+}
+
+static json_object* xx_report_socket_metric(int sockfd, int proto)
+{
+    char metric_name[256];
+    const char *t_row[SOCK_TBL_COL_MAX];
+    struct sockaddr_storage ss_local, ss_remote;
+    socklen_t ss_len;
+    json_object *jres_tbl_content = NULL;
+
+    if (sockfd == -1 || (proto != IPPROTO_TCP && proto != IPPROTO_UDP))
+	return NULL;
+
+    ss_len = sizeof(ss_local);
+    memset(&ss_local, 0, ss_len);
+    if (getsockname(sockfd, (struct sockaddr*) &ss_local, &ss_len))
+	return NULL;
+    ss_len = sizeof(ss_remote);
+    memset(&ss_remote, 0, ss_len);
+    if (getpeername(sockfd, (struct sockaddr*) &ss_remote, &ss_len))
+	return NULL;
+
+    memset(t_row, 0, sizeof(t_row));
+    snprintf(metric_name, sizeof(metric_name),
+	    "urn:ietf:metrics:perf:Priv_MPMonitor_Active_%s-ConnectionEndpoints__Multiple_Raw",
+	    (proto == IPPROTO_TCP) ? "TCP" : "UDP" );
+
+    t_row[socket_tbl_col_observer] = strdup("ma"); /* measurement-agent */
+    if (xx_nameinfo(&ss_local, sizeof(ss_local), NULL,
+                &t_row[socket_tbl_col_local_af],
+                &t_row[socket_tbl_col_local_addr],
+                &t_row[socket_tbl_col_local_port]))
+        goto err_exit;
+    if (xx_nameinfo(&ss_remote, sizeof(ss_remote), NULL,
+                &t_row[socket_tbl_col_remote_af],
+                &t_row[socket_tbl_col_remote_addr],
+                &t_row[socket_tbl_col_remote_port]))
+        goto err_exit;
+
+    json_object *jo, *jo1; /* ownership transferred to parent on _add */
+
+    /* TABLE CONTENT */
+    jres_tbl_content = json_object_new_object();  /* shall contain function, column, row arrays */
+    if (!jres_tbl_content)
+	goto err_exit;
+
+    /* table function object list */
+    jo = json_object_new_array();
+    jo1 = json_object_new_object();
+    assert(jo && jo1); /* FIXME */
+    json_object_object_add(jo1, "uri", json_object_new_string(metric_name));
+    json_object_array_add(jo, jo1);
+    json_object_object_add(jres_tbl_content, "function", jo);
+    jo = jo1 = NULL;
+
+    /* table columns list */
+    jo = json_object_new_array();
+    assert(jo); /* FIXME */
+    for (unsigned int i = 0; i < SOCK_TBL_COL_MAX; i++) {
+        json_object_array_add(jo, json_object_new_string(socket_report_col_names[i]));
+    };
+    json_object_object_add(jres_tbl_content, "column", jo);
+    jo = NULL;
+
+    /* one table row with our result data */
+    jo = json_object_new_object();
+    jo1 = json_object_new_array();
+    assert(jo && jo1); /* FIXME */
+    for (unsigned int i = 0; i < SOCK_TBL_COL_MAX; i++) {
+        json_object_array_add(jo1, json_object_new_string(t_row[i] ? t_row[i] : ""));
+    };
+    json_object_object_add(jo, "value", jo1);
+    jo1 = NULL;
+
+    /* table rows list */
+    jo1 = json_object_new_array();
+    assert(jo1);
+    json_object_array_add(jo1, jo);
+    jo = NULL;
+    json_object_object_add(jres_tbl_content, "row", jo1);
+    jo1 = NULL;
+
+err_exit:
+    for (unsigned int i = 0; i < SOCK_TBL_COL_MAX; i++)
+        free((void *)t_row[i]);
+
+    return jres_tbl_content;
+}
+
+int report_socket_metrics(TWAMPReport *report, int sockfd, int proto)
+{
+    struct twamp_report_private *rp;
+    json_object *jor;
+
+    if (!report)
+        return EINVAL;
+    if (!report->privdata)
+        return EINVAL;
+    rp = (struct twamp_report_private *)report->privdata;
+    if (!rp->root)
+        rp->root = json_object_new_array();
+    if (!rp->root)
+        return ENOMEM;
+
+    jor = xx_report_socket_metric(sockfd, proto);
+    if (!jor)
+        return ENOMEM;
+
+    json_object_array_add(rp->root, jor);
+    return 0;
+}
+
 int twamp_report(TWAMPReport *report, TWAMPParameters *param)
 {
     char metric_name[256];
+    struct twamp_report_private *report_private;
 
     assert(report);
     assert(param);
+
+    report_private = (struct twamp_report_private *)report->privdata;
 
     snprintf(metric_name, sizeof(metric_name),
 	    "Priv_MPMonitor_Active_UDP-Periodic-IntervalDurationMs%u-PackageCount%u-PackageSizeBytes%u__Multiple_Raw",
@@ -132,9 +317,90 @@ int twamp_report(TWAMPReport *report, TWAMPParameters *param)
     json_object_object_add(jres_tbl_content, "row", jarray_res_tbl_rows);
     jarray_res_tbl_rows = NULL;
 
-    fprintf(stdout, "%s\n", json_object_to_json_string_ext(jres_tbl_content, JSON_C_TO_STRING_PRETTY | JSON_C_TO_STRING_SPACED));
+    jo = report_private->root;
+    if (!jo)
+        jo = json_object_new_array();
+    assert(jo);
+
+    json_object_array_add(jo, jres_tbl_content);
+
+    /* we need to serialize the root array, but we don't want to output its delimiters [ ],
+     * and we need to omit the "," after the last member of the array */
+    int al = json_object_array_length(jo);
+    for (int i = 0; i < al ; i++) {
+        fprintf(stdout, "%s%s",
+                  json_object_to_json_string_ext(json_object_array_get_idx(jo, i),
+                                      JSON_C_TO_STRING_PRETTY | JSON_C_TO_STRING_SPACED),
+                  (i + 1 < al) ? ",\n" : "\n");
+    }
     fflush(stdout);
 
     return 0;
+}
+
+/**
+ * twamp_report_init() - allocates and initializes a TWAMPReport struct
+ *
+ * Allocates TWAMPReport, and TWAMPReport->result.  The allocations are
+ * zero-filled.
+ *
+ * Returns NULL on ENOMEM.
+ */
+TWAMPReport * twamp_report_init(void)
+{
+    struct twamp_report_private *rp = NULL;
+    TWAMPResult *tr = NULL;
+    TWAMPReport *r = NULL;
+    
+    tr = malloc(sizeof(TWAMPResult));
+    if (!tr)
+	goto err_exit;
+    memset(tr, 0, sizeof(TWAMPResult));
+
+    rp = malloc(sizeof(struct twamp_report_private));
+    if (!rp)
+        goto err_exit;
+    memset(rp, 0, sizeof(struct twamp_report_private));
+
+    r = malloc(sizeof(TWAMPReport));
+    if (!r)
+	goto err_exit;
+    memset(r, 0, sizeof(TWAMPReport));
+
+    r->result = tr;
+    r->privdata = rp;
+
+    return r;
+
+err_exit:
+    free(rp);
+    free(tr);
+    free(r);
+    return NULL;
+}
+
+/**
+ * twamp_report_done - deallocates a TWAMPReport
+ *
+ * frees all substructures/arrays and the TWAMPReport
+ *
+ * Handles NULL structs just fine.
+ */
+void twamp_report_done(TWAMPReport *r)
+{
+    struct twamp_report_private *p;
+    if (r) {
+	p = (struct twamp_report_private *)(r->privdata);
+	if (p) {
+	    if (p->root)
+		json_object_put(p->root);
+	    free(p);
+        }
+	if (r->result) {
+	    free(r->result->raw_data);
+	    free(r->result);
+	}
+	free(r);
+    }
 }
 
