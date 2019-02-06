40f95e3f24163d376742c1a16517d6e655216af5
Henrique de Moraes Holschuh <henrique@nic.br>
2019-02-06 16:22:27 -0200

    [MDC-403] simet-ma: simet-runner: disable stderr metric
    Disable the stderr metric, we will most probably replace it with a
    remote logging endpoint in the web API soon, at which point we will
    remove any left-over code.  Keep what we have right now since it
    could be useful as a guide.

--- simet-runner/src/templates.sh
+++ simet-runner/src/templates.sh
@@ -69,37 +69,44 @@ EOF2REPORTTEMPLATE
   :
 }
 
-error_template(){
-  cat << EOF1ERRORTEMPLATE
-{
-  "schedule": "$REPORT_SCHEDULE",
-  "action": "$_task_action",
-  "task": "$_task_name",
-  "parameters": $_task_parameters,
-  "option": $_task_options,
-  "conflict": [],
-  "tag": [
-    "task-version:$_task_version"${REPORT_MAC_ADDRESS_TAG_ENTRY}
-  ],
-  "event": "$REPORT_EVENT",
-  "start": "$_task_start",
-  "end": "$_task_end",
-  "status": $_task_status,
-  "table": [
-    { "function": [ { "uri": "urn:ietf:metrics:perf:Priv_SPMonitor_Passive_LineOutputStderr__Multiple_Raw" } ],
-      "column": [ "stderr_line" ],
-      "value": [
-EOF1ERRORTEMPLATE
-  find "$_task_dir" -type f -name "stderr.txt" | sort | while read -r i ; do
-    sed -e 's/[\]/\\\\/g' -e 's/"/\\"/g' -e 's/\t/\\t/g' \
-	-e 's/^/"/' -e '$ s/$/"/' -e '$! s/$/",/' -e 's/[[:cntrl:]]*//g' < "$i"
-  done
-  cat << EOF2ERRORTEMPLATE
-      ]
-    }
-  ]
-}
-EOF2ERRORTEMPLATE
-  :
-}
+# unimplemented right now
+#error_template() {
+#  :
+#}
+
+# do not use - metric unavailable.
+#
+#error_report_template(){
+#  cat << EOF1ERRORTEMPLATE
+#{
+#  "schedule": "$REPORT_SCHEDULE",
+#  "action": "$_task_action",
+#  "task": "$_task_name",
+#  "parameters": $_task_parameters,
+#  "option": $_task_options,
+#  "conflict": [],
+#  "tag": [
+#    "task-version:$_task_version"${REPORT_MAC_ADDRESS_TAG_ENTRY}
+#  ],
+#  "event": "$REPORT_EVENT",
+#  "start": "$_task_start",
+#  "end": "$_task_end",
+#  "status": $_task_status,
+#  "table": [
+#    { "function": [ { "uri": "urn:ietf:metrics:perf:Priv_SPMonitor_Passive_LineOutputStderr__Multiple_Raw" } ],
+#      "column": [ "stderr_line" ],
+#      "value": [
+#EOF1ERRORTEMPLATE
+#  find "$_task_dir" -type f -name "stderr.txt" | sort | while read -r i ; do
+#    sed -e 's/[\]/\\\\/g' -e 's/"/\\"/g' -e 's/\t/\\t/g' \
+#	-e 's/^/"/' -e '$ s/$/"/' -e '$! s/$/",/' -e 's/[[:cntrl:]]*//g' < "$i"
+#  done
+#  cat << EOF2ERRORTEMPLATE
+#      ]
+#    }
+#  ]
+#}
+#EOF2ERRORTEMPLATE
+#  :
+#}
 
