c9cc83cd727634a012b50e00feec1f4793eb9db7
Henrique de Moraes Holschuh <henrique@nic.br>
2019-04-17 13:55:04 -0300

    [MDC-111] simet-ma: simet-api: add enhanced wifi-based geolocation
    Add an enhanced wifi-based geolocation script that works on OpenWRT and
    straight Linux.
    
    Change simet-runner's geolocation task to be compatible with the new
    script.

--- simet-runner/src/task_geolocation.sh
+++ simet-runner/src/task_geolocation.sh
@@ -7,6 +7,7 @@
 #
 # function geolocate()
 # - input param: GEOLOCATE (simet_geolocation.sh full path)
+#   - needs simet_geolocation API v2 (LMAP-like output)
 # - output: json to stdout when successful
 #           simet_geolocation.sh debug output to stderr
 # - return 0, geolocate metric json sent to stdout
@@ -14,29 +15,13 @@
 #
 # Dependencies:
 # - simet_geolocation.sh
-# - jsonfilter (configured as $JSONFILTER)
-# - 64-bit integer shell math (bash ok, openwrt cc ok) for y2038, y2036(ntp)
 #
 ################################################################################
 
 geolocate() {
   GEOLOCATE=${GEOLOCATE:-simet_geolocation.sh}
-
-  command -v $GEOLOCATE >/dev/null 2>&1 || return 1
-  GEO=$($GEOLOCATE) || return 1
-  GEO_UTIME=$(echo "$GEO" | sed -n -e '1 p')
-  # Unix epoch to NTP epoch, simplified
-  GEO_NTIME=$((GEO_UTIME + 2208988800))
-  GEO_LAT=$(echo "$GEO" | sed -n -e '2 {s/ .*// ; p}')
-  GEO_LNG=$(echo "$GEO" | sed -n -e '2 {s/^ *[^ ]\+ *// ; s/ *[^ ]\+ *$// ; p}')
-  GEO_ACC=$(echo "$GEO" | sed -n -e '2 {s/^ *[^ ]\+ \+[^ ]\+ *// ; p}')
-  cat << EOFGEOLOCATE
-{
-  "function": [ { "uri": "Priv_SPMonitor_Passive_IEEE80211-BSSID-IPaddress-GeolocationProviderGoogle-MaxAccuracyM150__Multiple_Singleton" } ],
-  "column":   [ "timestamp", "latitude", "longitude", "accuracy" ],
-  "row": [ { "value": [ "$GEO_NTIME", "$GEO_LAT", "$GEO_LNG", "$GEO_ACC" ] } ]
-}
-EOFGEOLOCATE
+  GEOSCRIPT=$(command -v "$GEOLOCATE") || return 1
+  $GEOSCRIPT || return $?
   :
 }
 # keep line
