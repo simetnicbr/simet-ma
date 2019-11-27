simetnicbr/simet-ma

commit 135d15c4878f71e233cb47475a3ceeb446d014b0

[MDC-463] simet-ma: twamp: exit with non-ok status on excess packets
If we receive more packets than we are prepared to handle, change the
exit status to SEXIT_OUTOFRESOURCE.  Proceed normally otherwise, so we
still return results.

twamp-client-c/src/twamp.c
@@ -338,7 +338,9 @@ int twamp_run_client(TWAMPParameters param) {
             t_param.report->result->packets_sent, t_param.report->result->packets_received,
             t_param.report->result->packets_dropped_timeout);
 
-    rc = SEXIT_SUCCESS;
+    /* SEXIT_OUTOFRESOURCE if we got way too many duplicates, othetwise SEXIT_SUCCESS */
+    rc = (t_param.report->result->packets_received < t_param.param.packets_max) ?
+        SEXIT_SUCCESS : SEXIT_OUTOFRESOURCE;
 
 TEST_CLOSE:
     if (shutdown(fd_test, SHUT_RDWR) != 0) {

commit 5158462f44e268c743b8b729ec654497572ed6f1

[MDC-463] simet-ma: twamp: really accept 200% packets
Ensure we can actually receive 200% of the number of packets without
returning an error status, as that is an external requirement (unrelated
to TWAMP itself).

Remove the extra packet from the report, it is just a sentinel and
should not be reported.

Issue a warning when this happens.

Fixes: commit 135d15c4878f71e233cb47475a3ceeb446d014b0
       [MDC-463] simet-ma: twamp: exit with non-ok status on excess packets

twamp-client-c/src/report.c
@@ -272,7 +272,13 @@ int twamp_report(TWAMPReport *report, TWAMPParameters *param)
     /* each member of the tbl_rows below be a single "value: ["cell", "cell"]" array object? */
     json_object * jarray_res_tbl_rows = json_object_new_array();
 
-    for (unsigned int it = 0; it < report->result->packets_received; it++) {
+    /*
+     * NOTE: we do not report the sentinel packet
+     */
+    unsigned int np = report->result->packets_received;
+    if (np == param->packets_max)
+        np--;
+    for (unsigned int it = 0; it < np; it++) {
         ReportPacket pkg;
 
         struct timeval tv_sender = timestamp_to_timeval(&(report->result->raw_data[it].data.SenderTime));

twamp-client-c/src/twamp.c
@@ -58,6 +58,19 @@ int twamp_run_client(TWAMPParameters param) {
 
     assert(param.packets_count < param.packets_max);
 
+    /* Make room for one extra packet, which acts as a sentinel of
+     * too-many-dupes.
+     *
+     * This only exists because we have an external requirement that we
+     * have to be able to handle at least 100% packet duplication.
+     *
+     * NOTE: we do not process this last packet, it is received and
+     * stored, but discarded as lost when processing.  If you remove
+     * this line, you must adjust the post-receive routines to not
+     * ignore the last packet when packets_received == packets_max.
+     */
+    param.packets_max++;
+
     // Create TWAMPReport
     TWAMPReport * report = twamp_report_init();
     if (!report)
@@ -338,9 +351,13 @@ int twamp_run_client(TWAMPParameters param) {
             t_param.report->result->packets_sent, t_param.report->result->packets_received,
             t_param.report->result->packets_dropped_timeout);
 
-    /* SEXIT_OUTOFRESOURCE if we got way too many duplicates, othetwise SEXIT_SUCCESS */
-    rc = (t_param.report->result->packets_received < t_param.param.packets_max) ?
-        SEXIT_SUCCESS : SEXIT_OUTOFRESOURCE;
+    /* SEXIT_OUTOFRESOURCE if we got way too many duplicates, otherwise SEXIT_SUCCESS */
+    if (t_param.report->result->packets_received < t_param.param.packets_max) {
+        rc = SEXIT_SUCCESS;
+    } else {
+        rc = SEXIT_OUTOFRESOURCE;
+        print_warn("Received too many packets, test aborted with partial results");
+    }
 
 TEST_CLOSE:
     if (shutdown(fd_test, SHUT_RDWR) != 0) {