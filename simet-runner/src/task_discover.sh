simetnicbr/simet-ma

commit 7826b0086e4df450cb5ed527c4ef63b6ba9e1374

[MDC-1350] simet-runner: update ServiceList report metric
Add the GLOBAL_STATE_PEER_IDXMAP map with the latency-based server
selection result to the ServiceList debugging report metric.

This required some refactoring of templates.sh::task_json_template(),
to allow multiple columns from multiple JSON files.

During the refactoring, a bug in task_json_template() was found, and
fixed: it would not handle properly any LFs in the JSON, and would
generate multiple rows (with JSON fragments).  This will happen if the
Service List API ever decides to send formatted JSON output with line
breaks, but there is no real impact, since it is just debugging
information, and the end result should still be syntatically valid and
accepted as a measurement report.

simet-runner/src/task_discover.sh
@@ -47,7 +47,7 @@
 #
 ################################################################################
 
-_report_servicelist_output() {
+report_servicelist_output() {
   export _task_name="${LMAP_TASK_NAME_PREFIX}servicelist-output"
   export _task_version="$PACKAGE_VERSION"
   export _task_dir="$BASEDIR/report/0metadata-servicelist"
@@ -59,9 +59,10 @@ _report_servicelist_output() {
   export _task_status="0"
   export _task_end=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
   mkdir -p "$_task_dir/tables"
-  task_json_template "$BASEDIR/services.json" \
-	  "urn:ietf:metrics:perf:Priv_SPMonitor_Passive_ServiceList-output__Multiple_Raw" \
-	  "services_json" > "$_task_dir/result.json" || :
+  task_json_template "urn:ietf:metrics:perf:Priv_SPMonitor_Passive_ServiceList-output__Multiple_Raw" \
+      "$BASEDIR/services.json" "services_json" \
+      "$BASEDIR/services_reorder.json" "reordering_map" \
+    > "$_task_dir/result.json" || :
 }
 
 discover_init() {

simet-runner/src/main.sh
@@ -190,7 +190,7 @@ _main_orchestrate(){
   local _loopcounter=1
   local _collector_endpoint=
 
-  discover_init && subtask_serverselection
+  discover_init && subtask_serverselection && report_servicelist_output
   discover_next_peer
   while [ $? -eq 0 ]; do
     local _auth_endpoint="https://$(discover_service AUTHORIZATION HOST):$(discover_service AUTHORIZATION PORT)/$(discover_service AUTHORIZATION PATH)"

simet-runner/src/subtask_serversel.sh
@@ -12,11 +12,28 @@
 #  - input var: AGENT_ID, AGENT_TOKEN
 #  - input data: services.json, twampquick_parameters.json
 #  - output var: GLOBAL_STATE_PEER_IDXMAP
+#  - output file: services_reorder.json (for ServiceList report metric)
 #
 # Dependencies:
 # - jsonfilter (default installed at OpenWRT) as $JSONFILTER
 #
 ################################################################################
+
+_generate_reordermap_json() {
+  local outjson="$BASEDIR/services_reorder.json"
+  local map="$*"
+  {
+    printf '{ "rtt_serversel": [ '
+    while [ -n "$map" ] ; do
+      local e="${map%% *}"
+      map="${map#* }"
+      [ -z "$e" ] || [ "$e" -lt 0 ] && break
+      printf %s "$e, " || break
+    done | sed -e 's/, *$//'
+    printf '] }'
+  } > "$outjson"
+  :
+}
 
 _twquick() {
   local tw_tag="$1"
@@ -235,6 +252,7 @@ subtask_serverselection() {
   GLOBAL_STATE_PEER_IDXMAP=$(append_list "$i" "$FBIDXLIST" "-1" | \
     sed -e 's/^0\+\([0-9]\)/\1/' -e 's/ 0\+\([0-9]\)/ \1/g')
   #log_debug "server selection: ordered result: '$GLOBAL_STATE_PEER_IDXMAP'"
+  _generate_reordermap_json "$GLOBAL_STATE_PEER_IDXMAP"
   :
 }
 

simet-runner/src/templates.sh
@@ -6,6 +6,7 @@
 ################################################################################
 #
 # function task_template()
+# function task_json_template()
 # function report_template()
 # function error_template()
 #
@@ -65,9 +66,14 @@ EOF2TASKTEMPLATE
   :
 }
 
-# task_json_template "filename" "URN" "column name" ... > tables/result.json
+# task_json_template "URN" "filename" "column name" ["filename" "column name" ...] > tables/result.json
+# single row, one or more columns.  Tolerates empty/missing files (encodes as "null").
 task_json_template(){
+  [ $# -lt 3 ] && return
+
   _helper_measurement_context_tag
+  FURN="$1"
+  shift
   cat << EOF1TASKJSONTEMPLATE
 {
   "schedule": "$REPORT_SCHEDULE",
@@ -88,25 +94,33 @@ task_json_template(){
   "table": [
 EOF1TASKJSONTEMPLATE
 
-  while [ $# -ge 3 ] ; do
+  printf '{ "function": [ {"uri": "%s"} ], "column": [' "$FURN"
+  ( #subshell to preserve "$@"
+    local comma=
+    while [ $# -ge 2 ] ; do
+      [ -s "$1" ] && {
+	printf ' %s\"%s\"' "$comma" "$2"
+        comma=", "
+      }
+      shift 2
+    done
+  )
+  printf '], "row": [ {"value": [\n'
+  local comma=
+  while [ $# -ge 2 ] ; do
     FN="$1"
-    FURN="$2"
-    FCOL="$3"
-    shift 3
-
-    echo "{\"function\":[{\"uri\":\"$FURN\"}],"
-    echo " \"column\":[\"$FCOL\"],\"row\":["
-    sed -e 's/[\]/\\\\/g' -e 's/"/\\"/g' -e 's/\t/\\t/g' -e 's/^/{"value":["/' -e '$ s/$/"]}/' -e '$! s/$/"]},/' -e 's/[[:cntrl:]]*//g' \
-      < "$FN"
-  echo ']}'
-  [ $# -gt 0 ] && echo ','
-  :
+    if [ -s "$FN" ] ; then
+      [ -n "$comma" ] && printf ',\n'
+      printf '"'
+      sed -e 's/[\]/\\\\/g' -e 's/"/\\"/g' -e 's/\t/\\t/g' -e 's/[[:cntrl:]]*//g' \
+	< "$FN"
+      printf '"'
+      comma=1
+    fi
+    shift 2
+    :
   done
-
-  cat << EOF2TASKJSONTEMPLATE
-  ]
-}
-EOF2TASKJSONTEMPLATE
+  printf '\n]}\n]}\n]}\n'
   :
 }
 

commit c2de67bf975426091e7801395c44ae72a810455b

[MDC-1350] simet-runner: remove harmless left-over call to _report_servicelist_output
Remove a harmless left-over call to _report_servicelist_output(), which
was renamed and moved elsewhere in the previous commit.

This left-over call results in a stray error message to stderr from the
shell, but it doesn't abort the measurement.

Fixes: commit 7826b0086e4df450cb5ed527c4ef63b6ba9e1374
       [MDC-1350] simet-runner: update ServiceList report metric

simet-runner/src/task_discover.sh
@@ -118,7 +118,7 @@ discover_init() {
     --url "$_curl2_endpoint/$AGENT_ID" > "$BASEDIR/serversel/twampquick_parameters.json" \
   & _curl2_pid=$!
 
-  wait $_curl1_pid && _report_servicelist_output
+  wait $_curl1_pid
   wait $_curl2_pid && log_debug "Latency-based server selection parameters received"
 }
 