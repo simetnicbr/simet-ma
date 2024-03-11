8a6704e060b1053b489d7c9d3cd4570ea21852e0
Henrique de Moraes Holschuh <henrique@nic.br>
2024-03-11 13:29:51 -0300

    tcpaq: update copyright dates

--- inetup-client-c/src/tcpaq.c
+++ inetup-client-c/src/tcpaq.c
@@ -1,6 +1,6 @@
 /*
  * SIMET2 MA Internet Availability Measurement (inetup) client
- * Copyright (c) 2018,2019 NIC.br <medicoes@simet.nic.br>
+ * Copyright (c) 2018-2024 NIC.br <medicoes@simet.nic.br>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
