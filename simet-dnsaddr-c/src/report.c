simetnicbr/simet-ma

commit 928ad1fd865faeeda9b5866c6bdaaf199d2549d6

[MDC-1540] simet-dnsaddr: tolerate timeouts for dnssec
Don't abort the dnssec measurement on timeout (EAI_AGAIN): it is prone
to occour on the first query to BAD_NODE for unknown reasons, and since
we would bail out and drop the dnssec measurement on the first error,
this makes it impossible to even get enough data about the issue.

Change sdnsa_getaddrinfo() and sdnsa_getaddrinfo_error() to return -1 on
EAI_EAGAIN instead of SEXIT_FAILURE (positive values).  It will not
record the measurement, the only change is the return value.

For sdnsa_dnssec_query(), EAI_EAGAIN on any of the measurement steps
will still preclude further measurement steps, but it will not end the
measurement loop.

simet-dnsaddr-c/src/simet_dnsaddr.c
@@ -165,6 +165,7 @@ static const char *sdnsa_get_reflect_domain(int is_ip6, const char * const id, c
     return strdup(node);
 }
 
+/* -1 for timeout (EAI_AGAIN), 0 for OK, >0: SEXIT */
 static int sdnsa_getaddrinfo(int af, const char * node, struct dns_addrinfo_head *result)
 {
     /* we give a hint for SOCK_DGRAM UDP so that we don't get multiple answers for tcp, udp, raw... */
@@ -189,7 +190,7 @@ static int sdnsa_getaddrinfo(int af, const char * node, struct dns_addrinfo_head
 
     if (eai) {
         print_msg(MSG_TRACE, "failed to resolve %s: %s", node, gai_strerror(eai));
-        return SEXIT_FAILURE;
+        return (eai == EAI_AGAIN) ? -1 : SEXIT_FAILURE;
     } else {
         print_msg(MSG_TRACE, "getaddrinfo(%s) took %lld microseconds", node, delta_us);
     }
@@ -392,7 +393,7 @@ static int sdnsa_getaddrinfo_error(int af, const char * node, struct dns_addrinf
     /* For DNSSEC, we get either success, EAI_NODATA or EAI_NONAME
      * anything else is an unexpected failure (network error, etc) */
     if (eai != EAI_NODATA && eai != EAI_NONAME && eai != 0) {
-        return SEXIT_FAILURE;
+        return (eai == EAI_AGAIN) ? -1 : SEXIT_FAILURE;
     }
 
     if (result) {
@@ -525,6 +526,8 @@ static int sdnsa_dnssec_query(struct dns_addrinfo_head * const dnsres_dnssec_val
         if (!rc) {
             rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_UNSIGNED_NODE, NULL);
         }
+        /* don't stop on timeouts (EAI_AGAIN) */
+        rc = (rc > 0)? rc : 0;
     }
 
     return (!rc)? 0 : SEXIT_FAILURE;

commit c76260ec40f33d161dcfa1b31adf5a154ad76912

[MDC-1540] simet-dnsaddr: do not fail measurement if dnssec fails
The DNSSEC measurement is secondary, we should not fail the dnsaddr
measurement as a whole just because the DNSSEC measurement failed.

Fix sdnsa_dnssec_query() to properly detect empty result sets as a
failure, and change sdnsa_render_report() to ignore errors when
rendering the DNSSEC part of the report.

Fixes: commit 928ad1fd865faeeda9b5866c6bdaaf199d2549d6
       ([MDC-1540] simet-dnsaddr: tolerate timeouts for dnssec)

simet-dnsaddr-c/src/report.c
@@ -501,18 +501,10 @@ int sdnsa_render_report(struct dns_addrinfo_head * const data_priming,
     }
 
     /* Fill in rows with DNSSEC measurement data */
-    if (data_dnssec_valid && data_dnssec_valid->head) { /* optional */
-        rc = sdnsa_render_dnssec(jrows, "dnssec-valid", data_dnssec_valid);
-        if (rc) {
-            goto err_exit;
-        }
-
-        /* non-optional if we have dnssec_valid */
-        if (data_dnssec_invalid) {
-            rc = sdnsa_render_dnssec(jrows, "dnssec-invalid", data_dnssec_invalid);
-            if (rc) {
-                goto err_exit;
-            }
+    /* Optional measurement, so we don't care if it throws an error */
+    if (!is_empty_dns_addrinfo_list(data_dnssec_valid) && !is_empty_dns_addrinfo_list(data_dnssec_invalid)) {
+        if (!sdnsa_render_dnssec(jrows, "dnssec-valid", data_dnssec_valid)) {
+            sdnsa_render_dnssec(jrows, "dnssec-invalid", data_dnssec_invalid);
         }
     }
 

simet-dnsaddr-c/src/report.h
@@ -20,6 +20,7 @@
 #define REPORT_H_
 
 #include <inttypes.h>
+#include <stdbool.h>
 
 enum report_mode {
     SSPOOF_REPORT_MODE_FRAGMENT = 0, /* Array contents */
@@ -50,6 +51,11 @@ int sdnsa_render_report(struct dns_addrinfo_head * const data_priming,
                         struct dns_addrinfo_head * const data_dnssec_valid,
                         struct dns_addrinfo_head * const data_dnssec_invalid,
                         enum report_mode report_mode);
+
+static inline bool is_empty_dns_addrinfo_list(const struct dns_addrinfo_head * const l)
+{
+    return !(l && l->head);
+}
 
 #endif /* REPORT_H_ */
 

simet-dnsaddr-c/src/simet_dnsaddr.c
@@ -530,7 +530,9 @@ static int sdnsa_dnssec_query(struct dns_addrinfo_head * const dnsres_dnssec_val
         rc = (rc > 0)? rc : 0;
     }
 
-    return (!rc)? 0 : SEXIT_FAILURE;
+    return (!rc &&
+            !is_empty_dns_addrinfo_list(dnsres_dnssec_valid) &&
+            !is_empty_dns_addrinfo_list(dnsres_dnssec_invalid)) ? 0 : SEXIT_FAILURE;
 }
 
 /*