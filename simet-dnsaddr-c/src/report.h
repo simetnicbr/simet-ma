simetnicbr/simet-ma

commit 217ebbc86951cc87167cfe5ce6012512faa2280a

[MDC-1540] simet-dnsaddr: allow 3 failures on dnssec measurement
Give up trying to do the dnssec measurement only after three failures.

simet-dnsaddr-c/src/simet_dnsaddr.c
@@ -520,6 +520,7 @@ static int sdnsa_dnssec_query(struct dns_addrinfo_head * const dnsres_dnssec_val
      * Do it 10 times.  Fail the measurement early on error.
      */
     int rc = 0;
+    retries = 3;
     for (int i = 0; i < 10 && !rc; i++) {
         rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_GOOD_NODE, dnsres_dnssec_valid);
         if (!rc) {
@@ -529,8 +530,8 @@ static int sdnsa_dnssec_query(struct dns_addrinfo_head * const dnsres_dnssec_val
         if (!rc) {
             rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_UNSIGNED_NODE, NULL);
         }
-        /* don't stop on timeouts (EAI_AGAIN) */
-        rc = (rc > 0)? rc : 0;
+        /* don't stop on timeouts (EAI_AGAIN), accept 3 failures before giving up */
+        rc = (rc > 0 && retries-- > 0)? rc : 0;
     }
 
     return (!rc &&

commit d8d0d4ac4f151b31351b6966ec736705c2c70cd0

[MDC-1540] simet-dnsaddr: fix dnssec measurement retries
* Actually retry only three times for non-timeout errors
* Properly discard measurements where the control DNS query
  (SIMET_DNSSEC_UNSIGNED_NODE) failed before retrying
* Do the control query both before and after the two DNSSEC
  queries.

Fixes: commit 217ebbc86951cc87167cfe5ce6012512faa2280a
       [MDC-1540] simet-dnsaddr: allow 3 failures on dnssec measurement

simet-dnsaddr-c/src/report.h
@@ -58,6 +58,20 @@ static inline bool is_empty_dns_addrinfo_list(const struct dns_addrinfo_head * c
     return !(l && l->head);
 }
 
+static inline void append_dns_addrinfo_list_to_list(struct dns_addrinfo_head * const l, struct dns_addrinfo_head * const i) __attribute__((__unused__));
+static inline void append_dns_addrinfo_list_to_list(struct dns_addrinfo_head * const l, struct dns_addrinfo_head * const i)
+{
+    if (l && i && i->head && i->tail) {
+        if (l->head && l->tail) {
+            l->tail->next = i->head;
+            l->tail = i->tail;
+        } else {
+            l->head = i->head;
+            l->tail = i->tail;
+        }
+    }
+}
+
 #endif /* REPORT_H_ */
 
 /* vim: set et ts=8 sw=4 : */

simet-dnsaddr-c/src/simet_dnsaddr.c
@@ -522,16 +522,29 @@ static int sdnsa_dnssec_query(struct dns_addrinfo_head * const dnsres_dnssec_val
     int rc = 0;
     retries = 3;
     for (int i = 0; i < 10 && !rc; i++) {
-        rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_GOOD_NODE, dnsres_dnssec_valid);
+        struct dns_addrinfo_head q_valid = {};
+        struct dns_addrinfo_head q_invalid = {};
+
+        rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_UNSIGNED_NODE, NULL);
+        if (!rc) {
+            rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_GOOD_NODE, &q_valid);
+        }
         if (!rc) {
             /* Does not consider EAI_NODATA, EAI_NONAME or success an error */
-            rc = sdnsa_getaddrinfo_error(AF_INET, SIMET_DNSSEC_BAD_NODE, dnsres_dnssec_invalid);
+            rc = sdnsa_getaddrinfo_error(AF_INET, SIMET_DNSSEC_BAD_NODE, &q_invalid);
         }
         if (!rc) {
             rc = sdnsa_getaddrinfo(AF_INET, SIMET_DNSSEC_UNSIGNED_NODE, NULL);
         }
+
+        /* if this set of measurements was valid, store */
+        if (!rc) {
+            append_dns_addrinfo_list_to_list(dnsres_dnssec_valid, &q_valid);
+            append_dns_addrinfo_list_to_list(dnsres_dnssec_invalid, &q_invalid);
+        }
+
         /* don't stop on timeouts (EAI_AGAIN), accept 3 failures before giving up */
-        rc = (rc > 0 && retries-- > 0)? rc : 0;
+        rc = (rc > 0)? ((--retries > 0)? 0 : rc) : 0;
     }
 
     return (!rc &&