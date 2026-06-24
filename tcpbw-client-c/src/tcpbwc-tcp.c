simetnicbr/simet-ma

commit 31a650b51d718220022b091287f93e127c9a7692

tcpbw: allow adjustment of socket low watermarks
Add extended parameters tcp_rcvlowat and tcp_notsent_lowat to adjust the
TCP measurement socket's SO_RCVLOWAT and TCP_NOTSENT_LOWAT parameters.

Refer to socket(7) and also to:
https://lore.kernel.org/all/1374521803.4990.38.camel@edumazet-glaptop/

tcpbw-client-c/src/tcpbwc-tcp.c
@@ -320,6 +320,22 @@ static int create_measure_socket(const MeasureContext * const ctx, size_t * cons
 	print_warn("failed to enable TCP pacing (%u): %m", apace);
     }
 
+    /* enable socket watermarks, when requested */
+    uint32_t awatermark = (ctx->tcp_notsent_lowat > UINT32_MAX) ? UINT32_MAX : (uint32_t) ctx->tcp_notsent_lowat;
+#ifdef TCP_NOTSENT_LOWAT
+    if (awatermark > 0 && RETRY_EINTR(setsockopt(fd_measure, IPPROTO_TCP, TCP_NOTSENT_LOWAT, &awatermark, sizeof(awatermark)))) {
+	print_warn("failed to set TCP socket not-sent low watermark");
+    }
+#else
+    if (aswatermark > 0) {
+	print_warn("failed to enable TCP_NOTSENT_LOWAT: unavailable");
+    }
+#endif
+    awatermark = (ctx->tcp_rcvlowat > UINT32_MAX) ? UINT32_MAX : (uint32_t) ctx->tcp_rcvlowat;
+    if (awatermark > 0 && RETRY_EINTR(setsockopt(fd_measure, SOL_SOCKET, SO_RCVLOWAT, &awatermark, sizeof(awatermark)))) {
+	print_warn("failed to set TCP socket receive low watermark");
+    }
+
     return fd_measure;
 }
 

tcpbw-client-c/src/tcpbwc.c
@@ -121,6 +121,8 @@ static void print_usage(const char * const p, int mode)
 		"\t-X\textended measurement parameter(s) separated by blank or ;\n"
 		"\t\ttxdelay=n  inter-stream start delay (< 0: RTT/(-n*streams). >= 0: delay in us)\n"
 		"\t\tpacing=n   approximate per-stream pacing rate in KiB/s, 0: system default\n"
+		"\t\ttcp_rcvlowat=n       adjusts SO_RCVLOWAT, 0: system default\n"
+		"\t\ttcp_notsent_lowat=n  adjusts TCP_NOTSENT_LOWAT, 0: system default\n"
 		"\nserver URL: measurement server URL\n\n");
     }
     exit((mode)? SEXIT_SUCCESS : SEXIT_BADCMDLINE);
@@ -158,6 +160,20 @@ static int cmdline_extended_param(MeasureContext *ctx, char *param)
 	    data *= 1024; /* KiB/s -> bytes/s */
 	}
 	ctx->max_pacing_rate = (data > UINT32_MAX) ? UINT32_MAX : (uint32_t) data;
+	return 0;
+    } else if (!strcmp("tcp_notsent_lowat", param_name)) {
+	long data = atol(param_data);
+	if (data <= 0) {
+	    data = 0;
+	}
+	ctx->tcp_notsent_lowat = (data > INT32_MAX) ? INT32_MAX : (uint32_t) data;
+	return 0;
+    } else if (!strcmp("tcp_rcvlowat", param_name)) {
+	long data = atol(param_data);
+	if (data <= 0) {
+	    data = 0;
+	}
+	ctx->tcp_rcvlowat = (data > INT32_MAX) ? INT32_MAX : (uint32_t) data;
 	return 0;
     }
 

tcpbw-client-c/src/tcpbwc.h
@@ -98,7 +98,9 @@ typedef struct measure_context {
     unsigned int sample_period_ms;
     unsigned int stats_oversampling;
     int stream_start_delay;   /* n < 0: RTT/-n*stream_count; n >= 0: delay (us) */
-    unsigned int max_pacing_rate; /* 0: system default */
+    unsigned int max_pacing_rate;   /* 0: system default */
+    unsigned int tcp_rcvlowat;      /* 0: system default */
+    unsigned int tcp_notsent_lowat; /* 0: system default */
 
     size_t outgoing_mss;
     unsigned int rtt; /* smallest RTT (microseconds) */

commit bfa0dcc31dd92f3377351768ec694840b067063a

tcpbw: fix compilation when TCP_NOTSENT_LOWAT is undefined
Fix typo on the code used when TCP_NOTSENT_LOWAT is not defined in the
libc / headers.

Fixes: commit 31a650b51d718220022b091287f93e127c9a7692
       (tcpbw: allow adjustment of socket low watermarks)

tcpbw-client-c/src/tcpbwc-tcp.c
@@ -327,7 +327,7 @@ static int create_measure_socket(const MeasureContext * const ctx, size_t * cons
 	print_warn("failed to set TCP socket not-sent low watermark");
     }
 #else
-    if (aswatermark > 0) {
+    if (awatermark > 0) {
 	print_warn("failed to enable TCP_NOTSENT_LOWAT: unavailable");
     }
 #endif