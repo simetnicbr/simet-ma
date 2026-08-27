e19fb34bc1c59686fba9072296fbdf37e395ada8
Henrique de Moraes Holschuh <henrique@nic.br>
2026-08-27 08:08:21 -0300

    simet-api/utils: simet_hash: avoid harmless warning
    Avoid type conversions to print an error message in an error path.

--- simet-api/utils/src/simet_hash.c
+++ simet-api/utils/src/simet_hash.c
@@ -124,8 +124,8 @@ static int simet_do_hash_fd(int fd, const char * const fn)
 	    }
 	} while (res > 0 || (res == -1 && (errno == EINTR || errno == EAGAIN)));
 	if (res < 0) {
-        res = errno;
-        print_err("sha256: %s read failed: %s", fn, strerror(res));
+        const int e = errno;
+        print_err("sha256: %s read failed: %s", fn, strerror(e));
 	    return SEXIT_FAILURE;
 	} else if (size <= 0) {
         print_err("sha256: %s: no data to hash", fn);
